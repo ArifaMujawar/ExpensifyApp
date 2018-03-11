9546a59197d21c0cbfa2276c416b2e6a7dda8314
Arifa Mujawar <arifabegam.mujawar@gmail.com>
2018-03-11 13:06:31 +0200

    ES6 de-structuring

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -4,12 +4,18 @@ import {createStore}  from 'redux';
 const store = createStore((state = {count:0}, action) => {
     switch(action.type) {
         case 'INCREMENT': 
+            const incrementBy = typeof action.incrementBy ==='number' ? action.incrementBy : 1;
             return {
-                count:state.count+1    
+                count:state.count+incrementBy    
             };
         case 'DECREMENT':
+        const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
             return {
-                count: state.count-1
+                count: state.count-decrementBy
+            };
+        case 'SET':
+            return {
+                count:action.count
             };
         case 'RESET':
             return {
@@ -20,11 +26,14 @@ const store = createStore((state = {count:0}, action) => {
 
     }
 });
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());  
+});
 
-console.log(store.getState());
 
 store.dispatch({
-    type:'INCREMENT'
+    type:'INCREMENT',
+    incrementBy: 5
 });
 store.dispatch({
     type:'INCREMENT'
@@ -35,6 +44,12 @@ store.dispatch({
 store.dispatch({
     type:'DECREMENT'
 });
+store.dispatch({
+    type:'DECREMENT' ,
+    decrementBy:10
+});
+store.dispatch({
+    type:'SET' ,
+    count: 101
+});
 
-
-console.log(store.getState());
